359e70b396742170eabb3ebad3bae9307872ad42
Tookey <rachel.tookey@gmail.com>
2024-11-06 11:33:27 +0000

    Committing tasks06 and Fruit class

---
+++ TypesCS/Fruit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypesCS
+{
+    internal class Fruit
+    {
+        public string Name { get; set; } 
+
+        public string Colour { get; set; }
+
+        public Fruit(string name, string colour)
+        {
+            Name = name;
+            Colour = colour; 
+        }
+
+        public string myColour()
+        {
+            return this.Colour;
+        }
+
+
+
+    }
+}
---
+++ TypesCS/Task06.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypesCS
+{
+    internal class Task06
+    {
+        public static void userInput()
+        {
+            Console.WriteLine("Please input an int:");
+            string userInputInt = Console.ReadLine();
+            Console.WriteLine("Please input a decimal:");
+            string userInputDec = Console.ReadLine();
+
+            int inputInt = Int32.Parse(userInputInt);
+            decimal inputDec = Convert.ToDecimal(userInputDec);
+
+            decimal divResult = inputInt / inputDec;
+            double doubleResult = (double)divResult;
+
+            Console.WriteLine(doubleResult);
+
+
+        }
+
+        public static void userChar()
+        {
+            Console.WriteLine("Please input an string:");
+            string userString = Console.ReadLine();
+            Console.WriteLine("Would you like the answer returned as a char array?");
+            string userBool = Console.ReadLine();
+            bool val = bool.Parse(userBool);
+            
+            if (val)
+            {
+                char[] myArray = userString.ToCharArray();
+                Console.WriteLine(myArray);
+            }
+
+        }
+
+    }
+}
